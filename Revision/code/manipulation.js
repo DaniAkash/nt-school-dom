5dbddb5259f0d84cb0d2cba0a38097b94c591e12
DaniAkash <s.daniakash@gmail.com>
2023-06-23 20:31:36 +0530

    feat: added class manipulation

--- Revision/code/manipulation.js
+++ Revision/code/manipulation.js
@@ -2,16 +2,23 @@ const paragraphTag = document.createElement("p");
 
 const paragraphText = document.createTextNode("Paragraph Tag 2");
 
+// paragraphTag.innerText = "Paragraph Tag 2";
+
 paragraphTag.appendChild(paragraphText);
 
 document.body.appendChild(paragraphTag);
 
-const headingTag = document.querySelector("h1");
+// paragraphTag.remove();
+
+paragraphTag.classList.add("circle");
+paragraphTag.classList.add("error");
+
+// const headingTag = document.querySelector("h1");
 
 // console.log(headingTag.innerText);
 
 // headingTag.innerText = "Manipulation!";
 
-console.log(headingTag.innerHTML);
+// console.log(headingTag.innerHTML);
 
-headingTag.innerHTML = "Manipulation! <span>working</span>";
+// headingTag.innerHTML = "Manipulation! <span>working</span>";
